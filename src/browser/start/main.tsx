e040733ca0778a88847298840899bedf30600c21
seokju-na <seokju.me@gmail.com>
2019-08-31 23:46:59 +0900

    다크테마 지원

--- src/browser/start/main.tsx
+++ src/browser/start/main.tsx
@@ -1,6 +1,23 @@
+import { createMuiTheme } from '@material-ui/core';
+import { ThemeProvider } from '@material-ui/styles';
+import { remote } from 'electron';
 import React from 'react';
 import { render } from 'react-dom';
 import StartStepper from './containers/StartStepper';
+import './style.css';
+
+const isDarkMode = remote.systemPreferences.isDarkMode();
+const theme = createMuiTheme({
+  palette: {
+    type: isDarkMode ? 'dark' : 'light',
+  },
+});
+
+document.documentElement.style.backgroundColor = theme.palette.background.paper;
 
 const rootElement = document.getElementById('root')!;
-render(<StartStepper/>, rootElement);
+render((
+  <ThemeProvider theme={theme}>
+    <StartStepper/>
+  </ThemeProvider>
+), rootElement);
